caa92ea85b58b84e16a03165a64b6d157e422039
Chelsea S <csnider91@live.com>
2023-01-18 09:05:25 -0500

    first update

---
+++ main.js
@@ -0,0 +1,7 @@
+var button = document.querySelector("button");
+button.addEventListener("click", function() {
+    var image = document.getElementById("random-image");
+    image.src = "https://i.makeagif.com/media/4-21-2020/Idoraa.gif";
+    console.log("you clicked this button")
+
+});
